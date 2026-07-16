f0b37bb2d8d2215f5bdc176d03ec5c529188917e
Sumer <sumerrafey@gmail.com>
2026-07-16 17:51:55 +0530

    Make Linux install scripts use apt and dnf instead of dpkg and rpm directly and stop if not run with sudo

--- pynotes_debian_installer.sh
+++ pynotes_debian_installer.sh
@@ -1,9 +1,13 @@
+if [ "$EUID" -ne 0 ]; then
+  echo "error: run the script with sudo."
+  exit 1
+fi
 version="${1:-latest}"
 url_pynotes="https://raw.githubusercontent.com/rafugafu/PyNotes/main/v$version/PyNotes%20v$version.tar.gz"
 wget "$url_pynotes"
 tar -xf "PyNotes v$version.tar.gz"
 cd "PyNotes v$version"
-dpkg -i PyNotes.deb
+apt install ./PyNotes.deb
 cd ..
 rm -r "PyNotes v$version"
 rm "PyNotes v$version.tar.gz"
