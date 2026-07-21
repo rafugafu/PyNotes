5377eeaa4562bd68b65317d2340ca1a3d332c6ad
Sumer <sumerrafey@gmail.com>
2026-07-21 17:02:53 +0530

    Make Linux install scripts reinstall PyNotes if already installed and fix failing if the PyNotes v*.tar.gz file already exists

--- pynotes_debian_installer.sh
+++ pynotes_debian_installer.sh
@@ -4,10 +4,10 @@ if [ "$EUID" -ne 0 ]; then
 fi
 version="${1:-latest}"
 url_pynotes="https://raw.githubusercontent.com/rafugafu/PyNotes/main/v$version/PyNotes%20v$version.tar.gz"
-wget "$url_pynotes"
+wget -O "PyNotes v$version.tar.gz" "$url_pynotes"
 tar -xf "PyNotes v$version.tar.gz"
 cd "PyNotes v$version"
-apt install ./PyNotes.deb
+apt install --reinstall --allow-downgrades ./PyNotes.deb
 cd ..
 rm -r "PyNotes v$version"
 rm "PyNotes v$version.tar.gz"
