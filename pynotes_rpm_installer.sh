29f61b731a14da507bffb26557655dd45d927419
Sumer <sumerrafey@gmail.com>
2026-07-30 16:53:02 +0530

    Added guard around wrong version number, made the error messages colored red, and added a success message at the end in both the Linux install scripts

--- pynotes_rpm_installer.sh
+++ pynotes_rpm_installer.sh
@@ -1,13 +1,19 @@
+#!/bin/bash
 if [ "$EUID" -ne 0 ]; then
-  echo "error: run the script with sudo."
+  printf "\033[31merror: run the script with sudo.\033[0m\n"
   exit 1
 fi
 version="${1:-latest}"
 url_pynotes="https://raw.githubusercontent.com/rafugafu/PyNotes/main/v$version/PyNotes%20v$version.tar.gz"
-wget -O "PyNotes v$version.tar.gz" "$url_pynotes"
+if ! wget -O "PyNotes v$version.tar.gz" "$url_pynotes"; then
+  printf "\033[31merror: could not download PyNotes version '%s', check that it exists and is correct.\033[0m\n" "$version"
+  rm -f "PyNotes v$version.tar.gz"
+  exit 1
+fi
 tar -xf "PyNotes v$version.tar.gz"
 cd "PyNotes v$version"
 dnf reinstall --allow-downgrade pynotes*.rpm
 cd ..
-rm -r "PyNotes v$version"
-rm "PyNotes v$version.tar.gz"
+rm -rf "PyNotes v$version"
+rm -f "PyNotes v$version.tar.gz"
+printf '\n\033[32msuccessfully installed PyNotes v%s!\033[0m\n' "$version"
